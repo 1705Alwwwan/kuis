5928859188b374f4e07dde9b2acbedf6473a9b3f
Alwan Farhan Aziz <155723244+1705Alwwwan@users.noreply.github.com>
2025-10-18 05:40:25 +0700

    upgrade
    upgrade

--- app.js
+++ app.js
@@ -24,50 +24,12 @@ createApp({
           answer: "Pendidikan dan sosial"
         },
         {
-          question: "Nama sekolah yang berada di bawah naungan Yayasan Al Muslim adalah?",
-          options: ["Al Muslim School", "SD Negeri Mekarsari", "SMA Negeri 1 Tambun Selatan", "Pesantren Daarul Muttaqien"],
-          answer: "Al Muslim School"
-        },
-        {
-          question: "Nilai utama yang dipegang oleh Yayasan Al Muslim adalah?",
-          options: ["Disiplin, Tertib, dan Kreatif", "Islami, Berilmu, dan Mandiri", "Cerdas, Berakhlak, dan Berprestasi", "Modern, Mandiri, dan Terdepan"],
-          answer: "Islami, Berilmu, dan Mandiri"
-        },
-        {
           question: "Jenjang pendidikan yang tersedia di Yayasan Al Muslim mencakup?",
           options: ["PAUD hingga SMA", "TK hingga SMK", "SD hingga Universitas", "PG hingga Perguruan Tinggi"],
           answer: "PG hingga Perguruan Tinggi"
-        },
-        {
-          question: "Selain pendidikan formal, kegiatan apa yang juga dilakukan oleh Yayasan Al Muslim?",
-          options: ["Bisnis properti", "Ekspor-impor", "Sosial dan kemanusiaan", "Pariwisata religi"],
-          answer: "Sosial dan kemanusiaan"
-        },
-        {
-          question: "Ciri khas pendidikan di lingkungan Al Muslim adalah?",
-          options: [
-            "Berbasis teknologi dan akhlak Islami",
-            "Full boarding dan militeristik",
-            "Fokus hanya pada akademik",
-            "Nonformal dan bebas kurikulum"
-          ],
-          answer: "Berbasis teknologi dan akhlak Islami"
-        },
-        {
-          question: "Kegiatan sosial yang sering diadakan oleh Yayasan Al Muslim antara lain?",
-          options: ["Festival kuliner", "Bakti sosial dan santunan anak yatim", "Lomba otomotif", "Kegiatan politik lokal"],
-          answer: "Bakti sosial dan santunan anak yatim"
-        },
-        {
-          question: "Tujuan utama pendidikan di bawah Yayasan Al Muslim adalah?",
-          options: [
-            "Menyiapkan siswa menjadi pengusaha sukses",
-            "Membentuk generasi beriman, berilmu, dan berakhlak mulia",
-            "Melatih siswa menjadi pegawai negeri",
-            "Menghasilkan siswa berprestasi akademik tinggi saja"
-          ],
-          answer: "Membentuk generasi beriman, berilmu, dan berakhlak mulia"
         }
+        
+        
       ]
 
     }
